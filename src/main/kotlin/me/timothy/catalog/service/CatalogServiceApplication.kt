45458a7911157460964067300fc4bbe2d4af6499
Timothy Julian <timothyace73@gmail.com>
2025-09-28 23:40:19 +0700

    init

---
+++ src/main/kotlin/me/timothy/catalog/service/CatalogServiceApplication.kt
@@ -0,0 +1,11 @@
+package me.timothy.catalog.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatalogServiceApplication>(*args)
+}
---
+++ src/test/kotlin/me/timothy/catalog/service/CatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.timothy.catalog.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
