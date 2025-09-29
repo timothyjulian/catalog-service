5a420cce9801742230aff25dec2e6594ced311fa
Timothy Julian <timothyace73@gmail.com>
2025-09-29 23:27:12 +0700

    controller

---
+++ src/main/kotlin/me/timothy/catalog/service/contoller/GreetingController.kt
@@ -0,0 +1,17 @@
+package me.timothy.catalog.service.contoller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/greetings")
+class GreetingController {
+
+    @GetMapping("/{name}")
+    fun retrieveGreeting(@PathVariable("name") name : String) : String {
+        return "Hello $name";
+    }
+
+}
