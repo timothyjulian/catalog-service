ee450cdcf9a63f1fc6f16bea3e2465f3806ee421
Timothy Julian <timothyace73@gmail.com>
2025-09-29 23:42:51 +0700

    profiles yaml

--- src/main/kotlin/me/timothy/catalog/service/service/GreetingService.kt
+++ src/main/kotlin/me/timothy/catalog/service/service/GreetingService.kt
@@ -1,10 +1,14 @@
 package me.timothy.catalog.service.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingService {
 
-    fun retrieveGreeting(name: String) = "Hello $name"
+    @Value("\${message}")
+    lateinit var message: String
+
+    fun retrieveGreeting(name: String) = "$name, $message"
 
 }
