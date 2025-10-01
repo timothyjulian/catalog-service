619187791f796458f779621036d62f7da17a94cd
Timothy Julian <timothyace73@gmail.com>
2025-10-02 01:55:31 +0700

    test cases greeting controller

---
+++ src/test/integration/me/timothy/catalog/service/controller/GreetingControllerIntegrationTest.kt
@@ -0,0 +1,30 @@
+package me.timothy.catalog.service.controller
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class GreetingControllerIntegrationTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun retrieveGreeting() {
+        val name = "Timo"
+        val returnResult =
+            webTestClient.get().uri("/v1/greetings/{name}", name).exchange().expectStatus().is2xxSuccessful.expectBody(
+                String::class.java
+            ).returnResult()
+
+        Assertions.assertEquals("$name, Hello from default profile", returnResult.responseBody)
+    }
+
+}
