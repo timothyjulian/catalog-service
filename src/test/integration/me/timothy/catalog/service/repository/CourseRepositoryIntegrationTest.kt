733f18d6ec8ae56781d85cce87f821d3865dcf24
Timothy Julian <timothyace73@gmail.com>
2025-10-12 16:12:28 +0700

    add parameterized test for findCoursesByName method in CourseRepositoryIntegrationTest

--- src/test/integration/me/timothy/catalog/service/repository/CourseRepositoryIntegrationTest.kt
+++ src/test/integration/me/timothy/catalog/service/repository/CourseRepositoryIntegrationTest.kt
@@ -4,9 +4,13 @@ import me.timothy.catalog.service.util.courseEntityList
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -36,5 +40,26 @@ class CourseRepositoryIntegrationTest {
         println(courses)
 
         Assertions.assertEquals(2, courses.size)
+    }
+
+
+    @ParameterizedTest
+    @MethodSource(value = ["courseAndSize"])
+    fun findCoursesByName_approach2(name: String, expectedSize: Int) {
+        val courses = courseRepository.findCoursesByName(name)
+        println(courses)
+
+        Assertions.assertEquals(expectedSize, courses.size)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1)
+            )
+        }
     }
 }
